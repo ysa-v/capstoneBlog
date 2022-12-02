3c5089566d73e9bfb622703b60875a83130437dc
ysa-v <ysa.vandenberg@gmail.com>
2022-12-02 14:17:33 -0600

    first commit

---
+++ src/main/java/com/capstoneblog/capstoneblog/CapstoneBlogApplication.java
@@ -0,0 +1,13 @@
+package com.capstoneblog.capstoneblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstoneblog/capstoneblog/CapstoneBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstoneblog.capstoneblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
