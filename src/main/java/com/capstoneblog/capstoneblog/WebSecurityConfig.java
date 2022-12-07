1b612c07662c12630ea6756e254ddcd34fdcff59
Kathy Strebel <kthstrebel4@gmail.com>
2022-12-07 14:33:58 -0600

    Display login status on homepage

--- src/main/java/com/capstoneblog/capstoneblog/WebSecurityConfig.java
+++ src/main/java/com/capstoneblog/capstoneblog/WebSecurityConfig.java
@@ -4,13 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Service;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 @Configuration
 @EnableWebSecurity
@@ -19,10 +17,6 @@ public class WebSecurityConfig
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
-        // http.authorizeHttpRequests((requests) ->
-        // requests.anyRequest().authenticated()).httpBasic(withDefaults())
-        // .authenticationManager(new CustomAuthenticationManager());
-
         http.authorizeHttpRequests((requests) -> requests
                 .antMatchers("/", "/Post/View", "/unsecure")
                 .permitAll()
@@ -35,29 +29,6 @@ public class WebSecurityConfig
 
         return http.build();
     }
-
-    // @Bean
-    // WebSecurityCustomizer webSecurityCustomizer()
-    // {
-    // return (web) -> web.ignoring().antMatchers("/", "/Post/View/**");
-    // }
-
-    // @Bean
-    //// @Service("userDetailsService")
-    // public UserDetailsService userDetailsService()
-    // {
-    // UserDetails admin = User.withDefaultPasswordEncoder()
-    // .username("admin").password("password")
-    // .roles("ADMIN", "WRITER")
-    // .build();
-    //
-    // UserDetails writer = User.withDefaultPasswordEncoder()
-    // .username("writer").password("password")
-    // .roles("WRITER")
-    // .build();
-    //
-    // return new InMemoryUserDetailsManager(admin, writer);
-    // }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService()
@@ -73,5 +44,11 @@ public class WebSecurityConfig
                 .build();
 
         return new InMemoryUserDetailsManager(admin, writer);
+    }
+
+    @Bean
+    public SpringSecurityDialect SpringSecurityDialect()
+    {
+        return new SpringSecurityDialect();
     }
 }
