d77acd9c06f14c8e2bff2f4fd360fce8151b27f1
Kathy Strebel <kthstrebel4@gmail.com>
2022-12-07 14:05:24 -0600

    Add login accounts

--- src/main/java/com/capstoneblog/capstoneblog/WebSecurityConfig.java
+++ src/main/java/com/capstoneblog/capstoneblog/WebSecurityConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Service;
 
 @Configuration
 @EnableWebSecurity
@@ -17,20 +19,59 @@ public class WebSecurityConfig
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
-        http.authorizeHttpRequests(
-                (requests) -> requests.requestMatchers("/", "/home", "/Post/View/**").permitAll().anyRequest().authenticated())
-                .formLogin((form) -> form.loginPage("/login").permitAll())
+        // http.authorizeHttpRequests((requests) ->
+        // requests.anyRequest().authenticated()).httpBasic(withDefaults())
+        // .authenticationManager(new CustomAuthenticationManager());
+
+        http.authorizeHttpRequests((requests) -> requests
+                .antMatchers("/", "/Post/View", "/unsecure")
+                .permitAll()
+                .anyRequest()
+                .authenticated())
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll())
                 .logout((logout) -> logout.permitAll());
 
         return http.build();
     }
 
+    // @Bean
+    // WebSecurityCustomizer webSecurityCustomizer()
+    // {
+    // return (web) -> web.ignoring().antMatchers("/", "/Post/View/**");
+    // }
+
+    // @Bean
+    //// @Service("userDetailsService")
+    // public UserDetailsService userDetailsService()
+    // {
+    // UserDetails admin = User.withDefaultPasswordEncoder()
+    // .username("admin").password("password")
+    // .roles("ADMIN", "WRITER")
+    // .build();
+    //
+    // UserDetails writer = User.withDefaultPasswordEncoder()
+    // .username("writer").password("password")
+    // .roles("WRITER")
+    // .build();
+    //
+    // return new InMemoryUserDetailsManager(admin, writer);
+    // }
+
     @Bean
-    public UserDetailsService userDetailsService()
+    public InMemoryUserDetailsManager userDetailsService()
     {
-        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("Admin").password("password")
+                .roles("ADMIN", "WRITER")
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
+        UserDetails writer = User.withDefaultPasswordEncoder()
+                .username("Writer").password("password")
+                .roles("WRITER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, writer);
     }
 }
--- src/main/java/com/capstoneblog/capstoneblog/controllers/AuthController.java
+++ src/main/java/com/capstoneblog/capstoneblog/controllers/AuthController.java
@@ -3,11 +3,27 @@ package com.capstoneblog.capstoneblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/*********LOGINS*********
+ * 
+ * Username: Admin
+ * Password: password
+ * 
+ * Username: Writer
+ * Password: password
+ * 
+ * **********************/
+
 @Controller
 public class AuthController {
     @GetMapping("/login")
     public String logIn()
     {
-       return "login";
+        return "login";
     }
+    
+    //@GetMapping("/logout")
+    //public String logout()
+    //{
+    //    //
+    //}
 }
